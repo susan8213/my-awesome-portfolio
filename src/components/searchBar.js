951bf3fd5d6aeed50433f40ba823fb4f8e10158d
Ying Hsiu Lai <susan8213@gmail.com>
2021-01-06 16:55:09 +0800

    Add SearchBar component

---
+++ src/components/searchBar.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+import "./searchBar.css"
+
+const SearchBar = ({ setSearchQuery }) => {
+  const [searchString, setSearchString] = useState("")
+  const onChange = event => {
+    setSearchString(event.target.value)
+  }
+  return (
+    <div className="row search__bar">
+      <div className="col-6">
+        <input type="text" name="search" onChange={onChange} />
+      </div>
+      <div className="col-2">
+        <button
+          className="primary"
+          onClick={() => setSearchQuery(searchString)}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
